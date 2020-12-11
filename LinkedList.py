9de130f4d878dc9550568fb8c62a104577f25c38
Jared Lim <jaredlim320@gmail.com>
2020-12-10 23:35:55 -0600

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,20 @@
+class Node:
+	def __init__(self, val=0, next=None):
+    	self.val = val
+        self.next = next
+        
+        
+class LinkedList:
+	def __init__(self, head):
+    	self.head = head
+        
+    def printList(self):
+    
+    def insert(self, index):
+
+	def insertAtEnd(self):
+    
+    def remove(self, index):
+    
+    def copy(self):
+    
